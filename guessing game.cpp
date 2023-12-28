1bf4a7801ba5b1e49849fd0c5a0419d1aee08dd3
ben377-hash <133374475+ben377-hash@users.noreply.github.com>
2023-12-28 11:12:30 +0530

    Update guessing game.cpp

--- guessing game.cpp
+++ guessing game.cpp
@@ -1,40 +1,32 @@
 #include <iostream>
-#include <random>
 #include <ctime>
 
-using namespace std; // Using the std namespace
+using namespace std; 
 
 int main() {
-    // Use the current time as a seed for the random number generator
-    unsigned seed = time(0);
-
-    // Use the seed to seed the random engine
-    mt19937 gen(seed);
-
-
-    // Define the range for the random numbers (e.g., between 1 and 100)
-    uniform_int_distribution<int> distribution(1, 100);
-
-    // Defining input
+    srand(time(0));
+    int random_number = (rand() % 100) + 1;
+    cout<<"You have three chances: \n"<<endl;
     int num;
-    
-    cout<<"Enter any number from 1 to 100 "<<endl;
-    // Taking input
-    cin>>num; 
-
-    // Generate a random number
-    int random_number = distribution(gen);
-    
-    // Checking the input 
+    int x=0;
+    while(x<3){
+    cout<<"Enter the number: "<<endl;
+    cin>>num;
     if(num==random_number)
     {
         cout<<"Bingo! you guessed right"<<endl;
+        return 0;
     }
     else
     {
         cout<<"Oops! you guessed wrong "<<endl;
-        cout << "Random Number: " << random_number << endl;
+        
     }
+
+    x++;
+    }
+    cout << "Random Number was  " << random_number << endl;
+
     
     return 0;
 }
